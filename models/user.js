0d090218756149dee117ec1fd07ee70fc79d036e
AllanEl <allanrnelias@gmail.com>
2023-01-20 00:22:35 -0500

    initial files and code

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+// const assignmentSchema = require('./Assignment');
+
+// Schema to create Student model
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    thoughts: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    friends: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    // assignments: [assignmentSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const Student = model('student', studentSchema);
+
+module.exports = Student;
